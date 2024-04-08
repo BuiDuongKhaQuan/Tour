e059bc22d3ec6516180b20af94d85d181bf22ec2
BuiDuongKhaQuan <20130372@st.hcmuaf.edu.vn>
2024-04-08 16:06:47 +0700

    Update code: Account in admin

---
+++ src/pages/Admin/Account/Account.js
@@ -0,0 +1,217 @@
+import React, { useMemo } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Account.module.scss';
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
+import { Box } from '@mui/material';
+import Button from '~/components/Button';
+import { ContentCopy } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const data = [
+    {
+        name: {
+            firstName: 'Joshua',
+            lastName: 'Rolluffs',
+        },
+        address: '32188 Larkin Turnpike',
+        email: 'Charleston',
+        gender: 'South Carolina',
+    },
+    {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        address: '261 Erdman Ford',
+        email: 'East Daphne',
+        gender: 'Kentucky',
+    },
+    {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        address: '261 Erdman Ford',
+        email: 'East Daphne',
+        gender: 'Kentucky',
+    },
+    {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        address: '261 Erdman Ford',
+        email: 'East Daphne',
+        gender: 'Kentucky',
+    },
+    {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        address: '261 Erdman Ford',
+        email: 'East Daphne',
+        gender: 'Kentucky',
+    },
+    {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        address: '261 Erdman Ford',
+        email: 'East Daphne',
+        gender: 'Kentucky',
+    },
+    {
+        name: {
+            firstName: 'Jane',
+            lastName: 'Doe',
+        },
+        address: '769 Dominic Grove',
+        email: 'Columbus',
+        gender: 'Ohio',
+    },
+    {
+        name: {
+            firstName: 'Joe',
+            lastName: 'Doe',
+        },
+        address: '566 Brakus Inlet',
+        email: 'South Linda',
+        gender: 'West Virginia',
+    },
+    {
+        name: {
+            firstName: 'Kevin',
+            lastName: 'Vandy',
+        },
+        address: '722 Emie Stream',
+        email: 'Lincoln',
+        gender: 'Nebraska',
+    },
+    {
+        name: {
+            firstName: 'Joshua',
+            lastName: 'Rolluffs',
+        },
+        address: '32188 Larkin Turnpike',
+        email: 'Charleston',
+        gender: 'South Carolina',
+    },
+];
+export default function Account() {
+    const navigation = useNavigate();
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: 'name.firstName', //access nested data with dot notation
+                header: 'First Name',
+                size: 150,
+            },
+            {
+                accessorKey: 'name.lastName',
+                header: 'Last Name',
+                size: 150,
+            },
+            {
+                accessorKey: 'address', //normal accessorKey
+                header: 'Address',
+                size: 200,
+            },
+            {
+                accessorKey: 'email',
+                header: 'Email',
+                size: 150,
+                enableClickToCopy: true,
+                muiCopyButtonProps: {
+                    fullWidth: true,
+                    startIcon: <ContentCopy />,
+                    sx: { justifyContent: 'flex-start' },
+                },
+            },
+            {
+                accessorKey: 'gender',
+                header: 'Gender',
+                size: 150,
+            },
+        ],
+        [],
+    );
+
+    const table = useMaterialReactTable({
+        columns,
+        data,
+        muiTableBodyCellProps: ({ row }) => ({
+            //conditionally style selected rows
+            sx: {
+                fontSize: '15px',
+                '&:hover': {
+                    fontWeight: 'bold',
+                },
+            },
+        }),
+        muiTableCellProps: {
+            // Tăng kích thước của tất cả các ô trong bảng
+            sx: {
+                fontSize: '15px',
+            },
+        },
+        muiTableHeadCellProps: {
+            sx: {
+                fontWeight: 'normal',
+                fontSize: '20px',
+            },
+        },
+        muiPaginationProps: {
+            showRowsPerPage: true,
+            shape: 'rounded',
+        },
+        paginationDisplayMode: 'pages',
+        enableRowSelection: true,
+        renderTopToolbarCustomActions: ({ table }) => (
+            <Box sx={{ display: 'flex', gap: '1rem', p: '4px' }}>
+                <Button
+                    primary
+                    small
+                    color="secondary"
+                    onClick={() => {
+                        alert('Create New Account');
+                    }}
+                    variant="contained"
+                >
+                    Create Account
+                </Button>
+                <Button
+                    primary
+                    small
+                    color="error"
+                    disabled={!table.getIsSomeRowsSelected()}
+                    onClick={() => {
+                        alert('Delete Selected Accounts');
+                    }}
+                    variant="contained"
+                >
+                    Delete Selected Accounts
+                </Button>
+                <Button
+                    primary
+                    small
+                    color="error"
+                    disabled={!table.getIsSomeRowsSelected()}
+                    onClick={() => navigation('/admin-account-detail')}
+                    variant="contained"
+                >
+                    Edit Selected Accounts
+                </Button>
+            </Box>
+        ),
+    });
+
+    return (
+        <div className={cx('wrapper')}>
+            <MaterialReactTable table={table} />
+        </div>
+    );
+}
